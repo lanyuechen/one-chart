bfd437025e6bf4e6cf2f74ccc830a4189cd6a7f6
zhenglipeng <1252012805@qq.com>
2018-01-10 11:31:00 +0800

    优化

--- src/components/x-axis/axis-table.js
+++ src/components/x-axis/axis-table.js
@@ -21,18 +21,19 @@ class XAxis extends Component {
 
     const step = scale.step();
     const halfPaddingInner = scale.paddingInner() * step / 2;
-    const range = scale.range();
+    const axisWidth = scale.range()[1];
+    const axisHeight = AXIS_HEIGHT_BASIC * axis.length;
 
     return (
       <g className="one-axis" transform={`translate(${offset[0]}, ${offset[1]})`}>
 
-        <line x1={0.5} y1={0} x2={0.5} y2={AXIS_HEIGHT_BASIC * axis.length} />
-        <line x1={range[1] - 0.5} y1={0} x2={range[1] - 0.5} y2={AXIS_HEIGHT_BASIC * axis.length} />
-        <line x1={0.5} y1={AXIS_HEIGHT_BASIC * axis.length - 0.5} x2={range[1] - 0.5} y2={AXIS_HEIGHT_BASIC * axis.length - 0.5} />
+        <line x1={0.5} y1={0} x2={0.5} y2={axisHeight} />
+        <line x1={axisWidth - 0.5} y1={0} x2={axisWidth - 0.5} y2={axisHeight} />
+        <line x1={0.5} y1={axisHeight - 0.5} x2={axisWidth - 0.5} y2={axisHeight - 0.5} />
         {axis.map((d, i) => (
           <g key={i} transform={`translate(0, ${(axis.length - 1 - i) * AXIS_HEIGHT_BASIC})`}>
 
-            <line x1={0} y1={0.5} x2={range[1]} y2={0.5} />
+            <line x1={0} y1={0.5} x2={axisWidth} y2={0.5} />
 
             {d.map((dd, j) => {
               if (j === 0) {
