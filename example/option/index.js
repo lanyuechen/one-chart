f1e3e86a095c8ccfe4422c863bb87727ed0a1940
zhenglipeng <1252012805@qq.com>
2018-01-15 13:34:25 +0800

    小清理

--- example/option/index.js
+++ example/option/index.js
@@ -1,8 +1,7 @@
 import bar from './bar.json';
-import table from './table.json';
 import treemap from './treemap.json';
 import calendar from './calendar.json';
 
 export default {
-  bar, table, treemap, calendar
+  bar, treemap, calendar
 }
